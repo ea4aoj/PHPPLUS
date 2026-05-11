4a9b1c475315a82bdfb96f9d791e13a955d85852
Jose Luis Verdesoto <ea4aoj@gmail.com>
2026-05-11 10:53:56 +0200

    Update flightradar.php

--- html/flightradar.php
+++ html/flightradar.php
@@ -190,12 +190,11 @@ setInterval(() => {
 </button>
 
 
-<a href="mmdvm.php" class="btn btn-outline-light btn-sm">
-    <i class="bi bi-house-fill me-1"></i> Panel PHPPLUS
+<a href="mmdvm.php">
+    <button type="button">
+        <i class="bi bi-house-fill me-1"></i> Panel PHPPLUS
+    </button>
 </a>
-
-
-
 
 
 
