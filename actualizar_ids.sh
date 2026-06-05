c272f274c5d88da640dbb9fdcdfec503ef3b97f6
Jose Luis Verdesoto <ea4aoj@gmail.com>
2026-06-05 11:16:23 +0200

    Update actualizar_ids.sh

--- actualizar_ids.sh
+++ actualizar_ids.sh
@@ -6,8 +6,8 @@
                         sudo chmod 777 -R /home/pi/MMDVM_CM
                         cd /home/pi/MMDVMHost
                         curl --fail -o DMRIds.dat -s http://www.pistar.uk/downloads/DMRIds.dat
-                        sudo cp /home/pi/MMDVMHost/DMRIds.dat /home/pi/MMDVM_CM/DMR2YSF/DMRIds.dat
-                        sudo cp /home/pi/MMDVMHost/DMRIds.dat /home/pi/MMDVM_CM/YSF2DMR/DMRIds.dat
+                        cp /home/pi/MMDVMHost/DMRIds.dat /home/pi/MMDVM_CM/DMR2YSF/DMRIds.dat
+                        cp /home/pi/MMDVMHost/DMRIds.dat /home/pi/MMDVM_CM/YSF2DMR/DMRIds.dat
                         
                         echo "*********************************************"
                         echo "         IDS actualizadas correctamente"
